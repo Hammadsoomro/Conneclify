e177cd0d0b2030b7592958faf7f6ffdd6698e1f5
Hammadsoomro <165611609+Hammadsoomro@users.noreply.github.com>
2026-02-06 21:35:06 +0500

    Update Vite configuration and plugin imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import runtimeErrorOverlay from "@vitejs/plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(process.env.NODE_ENV !== "production"
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
+          await import("@vitejs/plugin-cartographer").then((m) =>
             m.cartographer(),
           ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
+          await import("@vitejs/plugin-dev-banner").then((m) =>
             m.devBanner(),
           ),
         ]
@@ -32,9 +31,11 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
+    host: "::",
+    port: 8080,
     fs: {
-      strict: true,
-      deny: ["**/.*"],
+      allow: ["./client", "./shared"],
+      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
 });
